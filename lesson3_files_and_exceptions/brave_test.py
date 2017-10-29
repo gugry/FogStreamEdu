994b603f888ada3b7e990ec0395d970444a3c87a
gugry <gugry@mail.ru>
2017-10-29 16:45:19 +1000

    Add brave_test.py

---
+++ lesson3_files_and_exceptions/brave_test.py
@@ -0,0 +1,18 @@
+# Написать консольную утилиту, передав которой путь к какой то папке,
+# она выводит список файлов и папок, которые есть в этой папке.
+# Используя библиотеки os и sys
+
+#!/usr/bin/env python3
+import os
+import sys
+print('Введите путь к папке')
+income = str (input())
+try:
+    list_of_dir = os.listdir(path="income")
+except FileNotFoundError:
+    print('Ошибка ввода, использована текущая папка')
+    list_of_dir = os.listdir(path="/")
+print(list_of_dir)
+for context in list_of_dir:
+    print(context)
+
