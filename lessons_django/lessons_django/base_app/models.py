e7e05b4de218397c1d4da2f05763745b57c62d13
Gugry <gugry@mail.ru>
2018-01-18 16:05:30 +1000

    Turn Author into ManyToManyField

--- lessons_django/lessons_django/base_app/models.py
+++ lessons_django/lessons_django/base_app/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 class Book(models.Model):
     name = models.CharField('Название', max_length=32)
-    author = models.ForeignKey('Author', verbose_name='Автор', on_delete='CASCADE', related_name='posts')
-    genre = models.ForeignKey('Genre', verbose_name='Жанр', on_delete='CASCADE', related_name='posts')
+    # author = models.ForeignKey('Author', verbose_name='Автор', on_delete='CASCADE', related_name='author')
+    author = models.ManyToManyField('Author', verbose_name='Автор', related_name='author')
+    genre = models.ForeignKey('Genre', verbose_name='Жанр', on_delete='CASCADE', related_name='genre')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         ordering = ('-name',)
@@ -16,6 +20,9 @@ class Author(models.Model):
     sername = models.CharField('Фамилия',max_length=32)
     patronymic = models.CharField('Отчество ',max_length=32)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ('-sername',)
         verbose_name = 'Автор'
@@ -24,6 +31,9 @@ class Author(models.Model):
 class Genre(models.Model):
     name = models.CharField('Жанр', max_length=32)
     description = models.CharField('Описание', max_length=128)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         ordering = ('-name',)
