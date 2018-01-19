f3da590c9c31392e9d177608c704087cbef45f99
gugry <gugry@mail.ru>
2018-01-19 22:56:56 +1000

    Turn base_app into library

--- lessons_django/lessons_django/base_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from lessons_django.base_app.models import Author, Book, Genre
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Genre)
