ce95f60d17ad315e360d83f9e6ecd4c9bc5163a2
gugry <gugry@mail.ru>
2018-03-28 14:58:53 +1000

    Style corrections

--- lessons_django/library/urls.py
+++ lessons_django/library/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('<str:sort_field>/',  views.index, name='index'),
     path('author/<int:author_id>/',  views.author, name='author'),
     path('genre/<int:genre_id>/',  views.genre, name='genre'),
-
-#? Я хочу, чтобы переходя в корень сайта я попадал на страницу авторов с сортировкой по имени
 ]
--- lessons_django/library/views.py
+++ lessons_django/library/views.py
@@ -3,27 +3,13 @@ from .models import Author, Book, Genre
 from django.http import HttpResponse
 from django.template import loader
 
-# def index(request, sort_field):
-#     author_list = Author.objects.order_by(sort_field)
-#     output = ', '.join([author.name + ' ' + author.sername for author in author_list])
-#     return HttpResponse(output)
-
 def index(request, sort_field='id'):
     author_list = Author.objects.order_by(sort_field)
     template = loader.get_template('library/index.html')
-    # output = ', '.join([author.name + ' ' + author.sername for author in author_list])
     context = {
         'author_list': author_list,
     }
-    return HttpResponse(template.render(context, request))
-
-# def author(request, author_id):
-#     author_qset = Author.objects.get(id= author_id)
-#     books_qset = author_qset.author_book.all()
-#     books = []
-#     for book in books_qset:
-#         books.append([book, book.genre])
-#     return HttpResponse(books[0])
+    return render(request, 'library/index.html', context)
 
 def author(request, author_id):
     author_qset = Author.objects.get(id=author_id)
@@ -33,6 +19,8 @@ def author(request, author_id):
         'author': author_qset,
         'books': books_qset
     }
+    return render(request, 'library/index.html', context)
+
     return HttpResponse(template.render(context, request))
 
 def genre(request, genre_id):
@@ -43,17 +31,10 @@ def genre(request, genre_id):
         'genre': genre_qset,
         'books': books_qset
     }
-    return HttpResponse(template.render(context, request))
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+    # return HttpResponse(template.render(context, request))
 
 
 
-# def index(request):
-#     return HttpResponse("Hello, world.")
+
